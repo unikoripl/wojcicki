cad61fdeb5a82d2af9e6ea87cc0b0a7e998447c9
unikoripl <chelm20@op.pl>
2019-02-05 17:23:18 +0100

    inicjacja wtyczki do wordpress

--- autorska.php
+++ autorska.php
@@ -3,4 +3,15 @@
 /*
 Plugin Name: Moja autorska wtyczka
 Description: Wtyczka, którą stworzyłem sam
+Version 1.0
+Author: Jan Wójcicki
 */
+
+function autorska_add_menu_(){
+    add_menu_page('Strona główna','autorska','administrator','autorska','autorska_glowna','','24');
+}
+add_action('admin_menu','autorska_addmenu');
+
+function autorska_glowna(){
+    echo "Napis"
+}
