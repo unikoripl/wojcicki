0206b96b4e665859b57bd781bf27342d6fadf87b
unikoripl <chelm20@op.pl>
2019-02-05 17:51:52 +0100

    działająca wtyczunia

--- autorska.php
+++ autorska.php
@@ -7,11 +7,11 @@ Version 1.0
 Author: Jan Wójcicki
 */
 
-function autorska_add_menu_(){
-    add_menu_page('Strona główna','autorska','administrator','autorska','autorska_glowna','','24');
+function autorska_add_menu(){
+    add_menu_page('Strona główna','autorska wtyczka','administrator','autorska','autorska_glowna','',12);
 }
-add_action('admin_menu','autorska_addmenu');
+add_action('admin_menu','autorska_add_menu');
 
 function autorska_glowna(){
-    echo "Napis"
+    echo "Napis";
 }
