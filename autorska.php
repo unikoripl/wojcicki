b00b3a43fd71ea0fde5eca362b2a5b11a59e2a32
unikoripl <chelm20@op.pl>
2019-02-11 18:40:32 +0100

    tworzenie i usuwanie tabeli

--- autorska.php
+++ autorska.php
@@ -8,8 +8,14 @@ Author: Jan Wójcicki
 */
 
 class autorska_czat{
+//dwie zmienne przechowujące obiekt i nazwę tabeli
+    private $wpdb;
+    private $table_name;
 
 function autorska_czat(){
+    global $wpdb;
+    $this->wpdb = $wpdb;
+    $this->table_name = $wpdb->prefix . 'autorska_posts';
     add_action('admin_menu', array( &$this, 'autorska_add_menu'));
 }
 function autorska_add_menu(){
@@ -17,6 +23,50 @@ function autorska_add_menu(){
 }
 
 function autorska_glowna(){
+    //dodanie pola autorska_action
+    if(isset($_POST['autorska_action'])) {
+        if($_POST['autorska_action'] == 'add') {
+            if($this->add_post($_POST['post_content'])) {
+                $notice = '<div class="notice notice-success">Dodano posta o treści: ' . $_POST['post_content'] . '</div>';
+            } else {
+                $notice = '<div class="notice notice-error">Nie dodano posta o treści: ' . $_POST['post_content'] . '</div>';
+            }
+        }
+    }
+//utworzenie formularza
+?>
+
+<div class="notice notice-success"><p>Dziękuję za zainstalowanie mojej wtyczki! Poniżej znajdziesz dostępne opcje.</p></div>
+<div class="tekstowy">
+    <h1>Wybierz co chcesz zrobić a następnie potwierdź przyciskiem ,,Start"</h1>
+</div>
+<h2>Formularz</h2>
+<p>Wybierz opcje:</p>
+<div class="pojemnik_czat">
+            <h2><span class="dashicons dashicons-admin-comments"></span>Autorski Czat</h2>
+            <?= isset($notice) ? $notice : '';?>
+<form method="POST">
+    <?= $edit ? '<input type="hidden" name="autorska_post_id" value="' . $edit->id . '" />' : ''; ?>
+    <input type="hidden" name="autorska_action" value="<?= $edit ? 'edit' : 'add'; ?>"/>
+    <label for="post_content">Treść posta</label><br>
+    <input type="text" name="post_content" value="<?= $edit ? $edit->post_content : ''; ?>"
+           placeholder="Treść posta"/>
+    <input type="submit" value="<?= $edit ? 'Edytuj' : 'Dodaj'; ?> post" class="button-primary"/>
+</form>    
+<?php
+function add_post($post_content) {
+    //sprawdzam czy nie pusty i czy jest zalogowany
+    if(trim($post_content) != '' && is_user_logged_in()){
+        $user_id = get_current_user_id();
+        $post_content = esc_sql($post_content);
+        $this->wpdb->insert( $this->table_name, array('user_id' => $user_id, 'post_content' => $post_content) );
+        return TRUE;
+    }
+    return FALSE;
+}
+function get_autorska_posts() {
+    return $this->wpdb->get_results("SELECT * FROM $this->table_name ORDER BY create_date DESC LIMIT 0,100");
+}
 }
 // rejestracja kodu autorska_activation w którym utworzymy tabelę
 register_activation_hook(__FILE__, 'autorska_activation');
