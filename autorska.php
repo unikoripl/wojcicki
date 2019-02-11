cdf93a02692d3e4169ce7ab8e1731c6670a49d5f
unikoripl <chelm20@op.pl>
2019-02-11 18:54:32 +0100

    dodanie mozliwosci edycji i usuwania postów

--- autorska.php
+++ autorska.php
@@ -31,41 +31,57 @@ function autorska_glowna(){
             } else {
                 $notice = '<div class="notice notice-error">Nie dodano posta o treści: ' . $_POST['post_content'] . '</div>';
             }
+            //edycja postów
+        } else if($_POST['autorska_action'] == 'edit') {
+            if($this->edit_post($_POST['autorska_post_id'],$_POST['post_content'])) {
+                $notice = '<div class="notice notice-success">Edytowano wiadomość o treści: ' . $_POST['post_content'] . '</div>';
+            } else {
+                $notice = '<div class="notice notice-error">Nie udało się zaktualizować wiadomości o treści: ' . $_POST['post_content'] . '</div>';
+            }
         }
     }
-//utworzenie formularza
-?>
-
-<div class="notice notice-success"><p>Dziękuję za zainstalowanie mojej wtyczki! Poniżej znajdziesz dostępne opcje.</p></div>
-<div class="tekstowy">
-    <h1>Wybierz co chcesz zrobić a następnie potwierdź przyciskiem ,,Start"</h1>
-</div>
-<h2>Formularz</h2>
-<p>Wybierz opcje:</p>
-<div class="pojemnik_czat">
-            <h2><span class="dashicons dashicons-admin-comments"></span>Autorski Czat</h2>
-            <?= isset($notice) ? $notice : '';?>
-<form method="POST">
-    <?= $edit ? '<input type="hidden" name="autorska_post_id" value="' . $edit->id . '" />' : ''; ?>
-    <input type="hidden" name="autorska_action" value="<?= $edit ? 'edit' : 'add'; ?>"/>
-    <label for="post_content">Treść posta</label><br>
-    <input type="text" name="post_content" value="<?= $edit ? $edit->post_content : ''; ?>"
-           placeholder="Treść posta"/>
-    <input type="submit" value="<?= $edit ? 'Edytuj' : 'Dodaj'; ?> post" class="button-primary"/>
-</form>    
-<?php
-function add_post($post_content) {
-    //sprawdzam czy nie pusty i czy jest zalogowany
-    if(trim($post_content) != '' && is_user_logged_in()){
-        $user_id = get_current_user_id();
-        $post_content = esc_sql($post_content);
-        $this->wpdb->insert( $this->table_name, array('user_id' => $user_id, 'post_content' => $post_content) );
-        return TRUE;
+    if(isset($_POST['autorska_delete'])) {
+        //usuwanie postów
+        if($this->delete_post($_POST['autorska_post_id'])) {
+            $notice = '<div class="notice notice-success">Usunięto wiadomość id: ' . $_POST['autorska_post_id'] . '</div>';
+        } else {
+            $notice = '<div class="notice notice-error">Nie usunięto wiadomość o id: ' . $_POST['autorska_post_id'] . '</div>';
+        }
     }
-    return FALSE;
+    //utworzenie formularza
+    ?>
+
+    <div class="notice notice-success"><p>Dziękuję za zainstalowanie mojej wtyczki! Poniżej znajdziesz dostępne opcje.</p></div>
+    <div class="tekstowy">
+        <h1>Wybierz co chcesz zrobić a następnie potwierdź przyciskiem ,,Start"</h1>
+    </div>
+    <h2>Formularz</h2>
+    <p>Wybierz opcje:</p>
+    <div class="pojemnik_czat">
+                <h2><span class="dashicons dashicons-admin-comments"></span>Autorski Czat</h2>
+                <?= isset($notice) ? $notice : '';?>
+    <form method="POST">
+        <?= $edit ? '<input type="hidden" name="autorska_post_id" value="' . $edit->id . '" />' : ''; ?>
+        <input type="hidden" name="autorska_action" value="<?= $edit ? 'edit' : 'add'; ?>"/>
+        <label for="post_content">Treść posta</label><br>
+        <input type="text" name="post_content" value="<?= $edit ? $edit->post_content : ''; ?>"
+               placeholder="Treść posta"/>
+        <input type="submit" value="<?= $edit ? 'Edytuj' : 'Dodaj'; ?> post" class="button-primary"/>
+    </form>    
+    <?php
+    function add_post($post_content) {
+        //sprawdzam czy nie pusty i czy jest zalogowany
+        if(trim($post_content) != '' && is_user_logged_in()){
+            $user_id = get_current_user_id();
+            $post_content = esc_sql($post_content);
+            $this->wpdb->insert( $this->table_name, array('user_id' => $user_id, 'post_content' => $post_content) );
+            return TRUE;
+        }
+        return FALSE;
 }
+//funkcja zwracająca tablicę wiadomości ograniczona do 50 rekordów
 function get_autorska_posts() {
-    return $this->wpdb->get_results("SELECT * FROM $this->table_name ORDER BY create_date DESC LIMIT 0,100");
+    return $this->wpdb->get_results("SELECT * FROM $this->table_name ORDER BY create_date DESC LIMIT 0,50");
 }
 }
 // rejestracja kodu autorska_activation w którym utworzymy tabelę
